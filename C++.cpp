215b359d47bc8cd476be6b3c685719b0ac773e81
Nexolotipie <168264917+Theanhtk11@users.noreply.github.com>
2026-07-13 14:20:43 +0700

    Add files via upload

---
+++ C++.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long double ld;
+const ld PI = 3.14;
+
+int main()
+{
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(nullptr);
+    ld x; cin >> x;
+    cout << fixed << setprecision(4) <<  x * 2 << "\n";
+    cout << fixed << setprecision(4) <<  x * 2 * PI << "\n";
+    cout << fixed << setprecision(4) <<  x * x * PI << "\n";
+    
+    return 0;
+}
